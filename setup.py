aad6002753e25cf3e0ed677927d10a23ec0e220b
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2024-05-14 16:30:50 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="face_tagger",
-    version="0.1.1",
+    version="0.1.2",
     author="Jonas A. Wendorf",
     description="Tags recognized faces in images in Adobe Bridge compatible format",
     long_description=long_description,
